dccf7cd52809dcac2201d9b168bb6788d92f8ca4
Tricia Swift <tricia.swift8@gmail.com>
2023-10-30 15:13:35 -0500

    Added Appointment Model and Class

--- deshawnapi/models/city.py
+++ deshawnapi/models/city.py
@@ -1,6 +1,5 @@
-from django.db import models
+from django.db import models  # Import base class from Django stdlib
 
 
-class City(models.Model):
-    name = models.CharField(max_length=155)
-
+class City(models.Model):  # Must inherit from this base class
+    name = models.CharField(max_length=155)  # Define all non-id fields
